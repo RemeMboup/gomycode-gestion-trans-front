2c60cfb6fe881cbdcc00c3f4013ebe80439e0685
Mariama <marememboup93@gmail.com>
2024-01-07 21:48:15 +0000

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import '../style/Style.css'
+import monImage from '../images/logo.png';
 export default class Header extends Component {
+
     render(){
         return (
-            <header className="main-header">
-               <div>Hello</div>
-            </header>
+            <div className="navbar">
+                <div className="container d-flex justify-content-between">
+                    <div className="flex-start">
+                        <img src={monImage} alt='image' />
+                        <span className="logo">GDS Transport
+                        
+                        </span>
+                    </div>
+                    <div className="flex-end">
+                        <span className="item active">Acceuil</span>
+                        <span className="item">A propos </span>
+                        <span className="item">Contact </span>
+                    </div>
+                </div>
+            </div>
+               
         )
     }
 }
